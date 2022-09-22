0c65b62e8e48fc842a4757e7b20e559eaec58e05
Nazarur <zhinchynn@gmail.com>
2022-09-22 22:05:52 +0300

    I added section menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,16 @@
 const checkpoint = 600;
-        let header_bg = 'transparent';
-        let opacity = 1;
+        let header_bg = '#292e36';
+        let opacity = 0;
         
         window.addEventListener("scroll", () => {
             const currentScroll = window.pageYOffset;
             if(currentScroll <= checkpoint) {
-                header_bg = 'transparent';
+                header_bg = 'sticky';
                 opacity = 1 - currentScroll / checkpoint;
             } else {
-                header_bg = '#000';
+                header_bg = 'fixed';
                 opacity = 0;
             }
         
-            document.querySelector(".header-nav").style.background = header_bg;
-            slides.forEach(slide => slide.getElementsByTagName('img')[0].style.opacity = opacity);
+            document.querySelector(".header-nav").style.position = header_bg;
         });
